92e78ae2340b07e53dc1cc4cfada79ca6088c637
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-07-29 13:22:37 +0530

    Time: 3 ms (18.59%) | Memory: 47.7 MB (57.40%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,22 @@
 class Solution {
     public void moveZeroes(int[] a) {
-        int l=0,r=0,n=a.length;
-        while(l<=r && r<n){
-            if(a[l]!=0){
+        int l = 0, r = 0, n = a.length;
+
+        while (r < n) {
+            // ১. l যদি non-zero কোনো জায়গায় থাকে, তবে l-কে সামনে বাড়াও
+            if (a[l] != 0) {
                 l++;
+                r = Math.max(r, l); // r যেন সবসময় l-এর সমান বা সামনে থাকে
+            } 
+            // ২. r যদি 0 ধরে রাখে, তবে non-zero খোঁজার জন্য r-কে সামনে বাড়াও
+            else if (a[r] == 0) {
                 r++;
-            }else if(a[r]==0){
-                r++;
-            }
-            else if(a[r]!=0 && a[l]==0){
-                int tep=a[l];
-                a[l]=a[r];
-                a[r]=tep;
+            } 
+            // ৩. যখন a[l] == 0 এবং a[r] != 0 পাবে, তখন Swap করো
+            else {
+                int temp = a[l];
+                a[l] = a[r];
+                a[r] = temp;
                 l++;
                 r++;
             }
