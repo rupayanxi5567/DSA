3c1bdc4e79ffd8a19e118ad0dbf0a214e470d865
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-07-31 16:26:35 +0530

    Time: 1 ms (99.92%) | Memory: 69.3 MB (21.03%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -3,32 +3,15 @@ class Solution {
         int n=a.length,s=0,min = Integer.MAX_VALUE;
         int l=0,r=0;
 
-        if(n==1 && a[0]>=t) return 1;
-        if(n==1 && a[0]<t) return 0;
-
-        
-
-        while(l<=r && r<n){
-            if(l==r){
-                s=a[l];
-            }else if(l+1==r){
-                s=a[l]+a[r];
-            }else{
-                s=s+a[r];
-            }
-
-            if(s<t){
-                r++;
-            }else{
+        while(r<n){
+            s=s+a[r];
+            while(s>=t){
                 int range=r-l+1;
-                if(min>range){
-                    min=range;
-                }
-                // s=s-a[l]-a[r];
-                s=s-a[l]-a[r];
+                min=Math.min(min,range);
+                s=s-a[l];
                 l++;
             }
+            r++;
         }
-        return min == Integer.MAX_VALUE ? 0 : min;        
-    }
+return min == Integer.MAX_VALUE ? 0 : min;    }
 }
