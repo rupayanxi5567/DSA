4b2369901417d577456085b758911332e555da62
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-07-31 15:22:15 +0530

    Time: 2 ms (19.59%) | Memory: 69.4 MB (21.03%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -29,7 +29,6 @@ class Solution {
                 l++;
             }
         }
-        if(min==Integer.MAX_VALUE)return 0;
-        return min;
+        return min == Integer.MAX_VALUE ? 0 : min;        
     }
 }
