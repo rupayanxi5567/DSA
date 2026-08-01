79f3f32807c5dc7dd85700b11e94eb017db1f4c2
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-08-01 23:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (62.21%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeElement(int[] a, int x) {
         int n=a.length,l=0,r=n-1,cout=0;
-        if(n==1 &&a[0]==x) return 0;
         while(l<r){
             while(l<n && l<r && a[l]!=x){
                 l++;
@@ -15,10 +14,7 @@ class Solution {
             }
                 
         }
-        // for(int i=0;i<=l;i++){
-        //     if(a[i]==x) break;
-        //     cout++;
-
-        // }
-        return (l < n && a[l] == x) ? l : l + 1;    }
+        
+        return (l < n && a[l] == x) ? l : l + 1;    
+        }
 }
