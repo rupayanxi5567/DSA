a236bfd721f78a35e4c7c3212bb6f1f217cdb34e
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-08-07 16:56:11 +0530

    Time: 4 ms (18.23%) | Memory: 46.9 MB (93.50%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -4,19 +4,18 @@ class Solution {
         int l=1,r=0;
         int n=a.length;
         for(int i=1;i<n-1;i++){
-            if(n!=1 && a[i]>a[i+1] && a[i]>a[i-1]){
+            if(a[i]>a[i+1] && a[i]>a[i-1]){
                 l=i-1;
                 r=i+1;
 
-                while(l>=1 && r<n && a[l]>a[l-1]){
+                while(l>=1 && a[l]>a[l-1]){
                     l--;
                 }
 
-                while(l>=0 && r<n-1 && a[r]>a[r+1]){
+                while(r<n-1 && a[r]>a[r+1]){
                     r++;
                 }
             }
-            
             
             if(l>=0 && r<n){
                 int len=r-l+1;
