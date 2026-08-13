62f27fdf325689e3601fac48c821076df58430ab
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-08-13 11:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (56.06%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode h) {
+        if(h==null || h.next==null) return h;
+        ListNode d=new ListNode(0);
+        d.next=h;
+
+        ListNode t=d;
+        ListNode p=h;
+        ListNode q=p.next;
+
+        while(q!=null){
+            // if(q.next!=null && p.val==q.val && q.val==q.next.val){
+            //     t.next=q.next.next;
+            //     if(t.next==null) return d.next;
+            //     p=t.next;
+            //     q=p.next;
+            // }else 
+            
+            
+            
+            if(p.val==q.val){
+                // if(q.next!=null && q.val==q.next.val){
+                //     t.next=q.next.next;
+                //     if(t.next.val==q.val)t.next=t.next.next; 
+                // }else 
+                t.next=q.next;
+                if(t.next==null) return d.next;
+                while(t.next!=null && t.next.val==q.val)t.next=t.next.next; 
+                if(t.next==null) return d.next;
+                p=t.next;
+                q=p.next;
+            }else{
+                p=p.next;
+                q=q.next;
+                t=t.next;
+            }
+            
+        }
+        return d.next;
+    }
+}
