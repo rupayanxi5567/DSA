cbfc54b4725b064e7b3863d1e614ba3d2e85c6ee
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-09-04 00:08:50 +0530

    Time: 1 ms (87.91%) | Memory: 45.5 MB (45.89%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -4,7 +4,7 @@ class Solution {
         a[i]=a[j];
         a[j]=t;
     }
-    public void x(int j,List<List<Integer>>r,int n,int []a ){
+    public void prints(int j,List<List<Integer>>r,int []a,int n){
         if(j==n){
             List<Integer>al=new ArrayList<>();
             for(int x:a){
@@ -15,14 +15,14 @@ class Solution {
         }
         for(int i=j;i<n;i++){
             swaps(i,j,a);
-            x(j+1,r,n,a);
+            prints(j+1,r,a,n);
             swaps(i,j,a);
         }
     }
     public List<List<Integer>> permute(int[] a) {
         List<List<Integer>>r=new ArrayList<>();
         int n=a.length;
-        x(0,r,n,a);
+        prints(0,r,a,n);
         return r;
     }
 }
