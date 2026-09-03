a81a3179eb57405606a2f8592c81816cdaad6959
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-09-03 17:10:25 +0530

    Time: 1 ms (87.91%) | Memory: 45.5 MB (45.89%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,28 @@
 class Solution {
-    public void swaps(int i,int j,int []a){
+    public void swaps(int i,int j, int[] a){
         int t=a[i];
         a[i]=a[j];
         a[j]=t;
     }
-    public void prints(int j,int[] a,List<List<Integer>>r,int n){
+    public void x(int j,List<List<Integer>>r,int n,int []a ){
         if(j==n){
             List<Integer>al=new ArrayList<>();
             for(int x:a){
                 al.add(x);
-            }            
+            }
             r.add(new ArrayList<>(al));
             return;
         }
         for(int i=j;i<n;i++){
-                swaps(i,j,a);
-                prints(j+1,a,r,n);
-                swaps(i,j,a);
+            swaps(i,j,a);
+            x(j+1,r,n,a);
+            swaps(i,j,a);
         }
     }
     public List<List<Integer>> permute(int[] a) {
         List<List<Integer>>r=new ArrayList<>();
         int n=a.length;
-        prints(0,a,r,n);
+        x(0,r,n,a);
         return r;
     }
 }
