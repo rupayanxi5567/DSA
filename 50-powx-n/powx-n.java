be935b199bb4558f43a7eb456b077fdaff588b89
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-09-03 18:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (78.17%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
+    public double calc(double x,long n){
+            if(n==0)return 1;
+            if(n%2==0){
+                return calc(x*x,n/2);
+            }else{
+                return x*calc(x,n-1);
+            }
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long ln=n;
+        if(n<0){
+            x=1/x;
+            ln=-ln;
+        }
+        return calc(x,ln);
     }
 }
