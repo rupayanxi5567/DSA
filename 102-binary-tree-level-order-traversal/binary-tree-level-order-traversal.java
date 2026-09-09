d7e724f03956a4dac11c444d4846906a7737e0b3
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-09-09 13:39:00 +0530

    Time: 1 ms (95.75%) | Memory: 47 MB (7.75%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,28 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>>r=new ArrayList<>();
-        if(root==null){
-            return r;
-        }
-        Deque<TreeNode> q=new ArrayDeque<>();
+        if(root==null)return r;
+        Deque<TreeNode>q=new ArrayDeque<>();
         q.addLast(root);
-        while (!q.isEmpty()){
-            int sizeOfLevel=q.size();
+        while(!q.isEmpty()){
+            int sizes=q.size();
             List<Integer>al=new ArrayList<>();
-            for(int i=0;i<sizeOfLevel;i++){
-                TreeNode currNode=q.pollFirst();
-                al.add(currNode.val);
-                if(currNode.left!=null){
-                    q.addLast(currNode.left);
+            for(int i=1;i<=sizes;i++){
+                TreeNode cn=q.pollFirst();
+                al.add(cn.val);
+                if(cn.left!=null){
+                    q.addLast(cn.left);
                 }
-                if(currNode.right!=null){
-                    q.addLast(currNode.right);
+                if(cn.right!=null){
+                    q.addLast(cn.right);
                 }
             }
             r.add(al);
         }
         return r;
     }
-
-
 }
