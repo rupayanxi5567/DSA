4df1a373ba9f65dd8c525b7e065e3443a1893e80
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2026-09-14 18:54:56 +0530

    Time: 1 ms (97.52%) | Memory: 46.6 MB (24.29%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,5 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Definition for a binary tree node.
@@ -17,25 +16,29 @@ import java.util.Arrays;
  * }
  */
 class Solution {
-
-    public TreeNode buildTree(int[] p, int[] in) {
-        if (p.length == 0 || in.length == 0) {
-            return null;
+    int preIndex=0;
+    Map<Integer,Integer>inMap=new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i=0;i<inorder.length;i++){
+            inMap.put(inorder[i],i);
         }
-        TreeNode roots=new TreeNode(p[0]);
-        int index=0;
-        int rootVal= roots.val;
-        for(int i=0;i<in.length;i++){
-            if(in[i]==rootVal){
-                index=i;
-                break;
-            }
-        }
-        roots.left=buildTree(Arrays.copyOfRange(p,1,index+1),Arrays.copyOfRange(in,0,index));
-        roots.right=buildTree(Arrays.copyOfRange(p,index+1,p.length),Arrays.copyOfRange(in,index+1,in.length));
-
-        return roots;
+        return builds(preorder,0,inorder.length-1);
     }
 
+    private TreeNode builds(int[] preorder, int inS, int inE) {
+        if(inS>inE){
+            return null;
+        }
 
+        int rootVal=preorder[preIndex++];
+        TreeNode roots=new TreeNode(rootVal);
+
+        int inIndex=inMap.get(rootVal);
+
+        roots.left=builds(preorder,inS,inIndex-1);
+        roots.right=builds(preorder,inIndex+1,inE);
+
+        return roots;
+
+    }
 }
